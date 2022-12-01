925fe8596351431b4a86d6bd9155432279d49be6
Pratik Datey <pratikvdatey@gmail.com>
2022-12-01 22:45:09 +0530

    first commit

---
+++ Lazyme.py
@@ -0,0 +1,203 @@
+from sklearn.metrics import r2_score,accuracy_score,mean_squared_error,mean_absolute_error,classification_report,confusion_matrix,precision_score,recall_score,roc_curve,auc
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.linear_model import LogisticRegression,LinearRegression,PassiveAggressiveClassifier,PassiveAggressiveRegressor,Lasso,RidgeClassifier
+from sklearn.ensemble import ExtraTreesClassifier,RandomForestClassifier,RandomForestRegressor,ExtraTreesRegressor,GradientBoostingRegressor,VotingRegressor
+from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
+from sklearn.svm import SVC,SVR
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier,KNeighborsRegressor
+import warnings
+warnings.filterwarnings('ignore')
+
+
+#Classifiaction models
+logreg=LogisticRegression(random_state=42)
+rfc=RandomForestClassifier(random_state=42)
+exc=ExtraTreesClassifier(random_state=42)
+dc=DecisionTreeClassifier(random_state=42)
+svc=SVC(random_state=42)
+gbc=GaussianNB()
+knn=KNeighborsClassifier()
+pacc=PassiveAggressiveClassifier(random_state=42)
+rc=RidgeClassifier(random_state=42)
+
+
+
+#Regression models
+linereg=LinearRegression()
+rfr=RandomForestRegressor(random_state=42)
+ext=ExtraTreesRegressor(random_state=42)
+gbr=GradientBoostingRegressor(random_state=42)
+svr=SVR()
+knnr=KNeighborsRegressor()
+dtr=DecisionTreeRegressor(random_state=42)
+pacr=PassiveAggressiveRegressor(random_state=42,average=3)
+las=Lasso(random_state=42)
+
+
+
+name=[pacc,dc,rfc,exc,logreg,rc,knn,svc,gbc]
+name_r=[pacr,gbr,dtr,rfr,ext,las,knnr,linereg,svr]
+
+class smart_classifier:
+    def __init__(self,x,y):
+        self.__x=x
+        self.__y=y
+        
+    
+    def accuracy_score(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}     :'.format(i),accuracy_score(testy,pred))
+            print()
+            
+    def cross_validation(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            score=cross_val_score(i,self.__x,self.__y,cv=10)
+            print('{}     :'.format(i),score.mean())
+            print()
+            
+    def precision_score(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        if (trainy.nunique())>2: # Multiclass
+            print('It is multi-classed problem so for that we are calculating Presicion Score in average = "macro"')
+            print()
+            print()
+
+            for i in name:
+                i.fit(trainx,trainy)
+                pred=i.predict(testx)
+                print('{}    :'.format(i),precision_score(testy,pred,average='macro'))
+                print()
+
+        else:
+            for i in name:
+                i.fit(trainx,trainy) # Binary
+                pred=i.predict(testx)
+                print('{}    :'.format(i),precision_score(testy,pred))
+                print()
+           
+        
+            
+            
+
+    def recall_score(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        if (trainy.nunique())>2: # Multiclass
+            print('It is multi-classed problem so for that we are calculating Recall Score in average = "macro"')
+            print()
+            print()
+
+            for i in name:
+                i.fit(trainx,trainy)
+                pred=i.predict(testx)
+                print('{}    :'.format(i),recall_score(testy,pred,average='macro'))
+                print()
+
+        else:
+            for i in name:
+                i.fit(trainx,trainy) # Binary
+                pred=i.predict(testx)
+                print('{}    :'.format(i),recall_score(testy,pred))
+                print()
+
+            
+            
+    def classification_report(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('Model = {} :'.format(i))
+            print(classification_report(testy,pred),end='')
+            print()
+            
+    def confusion_matrix(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print ('{}:'.format(i))
+            print(confusion_matrix(testy,pred))
+            print()
+            
+    def mean_squared_error(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}    :'.format(i),mean_squared_error(testy,pred))
+            print()
+            
+            
+    def mean_absolute_error(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}    :'.format(i),mean_absolute_error(testy,pred))
+            print()
+            
+    def overfitting(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name:
+            i.fit(trainx,trainy)
+            pred_train=i.predict(trainx)
+            pred_test=i.predict(testx)
+            print('Training Accuracy of {}    :'.format(i),accuracy_score(trainy,pred_train))
+            print('Testing Accuracy  of {}    :'.format(i),accuracy_score(testy,pred_test))
+            print()
+            
+class smart_regressor:
+    
+    def __init__(self,x,y):
+        self.__x=x
+        self.__y=y
+        
+    def r2_score(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name_r:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}    :'.format(i),r2_score(testy,pred))
+            print()
+            
+    
+            
+    def cross_validation(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name_r:
+            i.fit(trainx,trainy)
+            score=cross_val_score(i,self.__x,self.__y,cv=10)
+            print('{}     :'.format(i),score.mean())
+            print()
+            
+    def mean_squared_error(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name_r:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}    :'.format(i),mean_absolute_error(testy,pred))
+            print()
+            
+    def mean_absolute_error(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name_r:
+            i.fit(trainx,trainy)
+            pred=i.predict(testx)
+            print('{}    :'.format(i),mean_absolute_error(testy,pred))
+            print()
+            
+    def overfitting(self):
+        trainx,testx,trainy,testy=train_test_split(self.__x,self.__y,test_size=0.25,random_state=42)
+        for i in name_r:
+            i.fit(trainx,trainy)
+            pred_train=i.predict(trainx)
+            pred_test=i.predict(testx)
+            print('Training Accuracy of {}    :'.format(i),r2_score(trainy,pred_train))
+            print('Testing Accuracy  of {}    :'.format(i),r2_score(testy,pred_test))
+            print()
